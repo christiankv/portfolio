2327826e1749dfbc0f071fa0dea52f959c89a1c4
christiankv <christian.kodehode@gmail.com>
2022-02-10 14:47:50 +0100

    s

--- javascript/drumkit/drumkit.js
+++ javascript/drumkit/drumkit.js
@@ -10,65 +10,60 @@ const sounds = [
 ]
 const body = document.querySelector("body")
 const container = document.querySelector(".container")
-console.log(body);
 
 
 body.addEventListener("keydown", (e)=> playsound(e))
-
-
-
 body.addEventListener("mousedown",(e) => playsound(e))
 
-function playsound(e){
-    {
-        console.log(e);
-        for (sound in sounds){
-            sounds[sound].pause()
-            sounds[sound].currentTime = 0;
-        }
-        console.log(e.code); 
-       let keypress = e.code
-        if (keypress === "KeyA" | e.target.id == "openhat"){
-            openhat.play()
-            document.getElementById("openhat").style.boxShadow = "gray 10px 30px 5px"
 
-        } else if (keypress === "KeyS" | e.target.id == "tom"){
-            document.getElementById("tom").style.boxShadow = "gray 10px 30px 5px"
-            tom.play()
-
-        }else if (keypress === "KeyD" | e.target.id == "clap"){
-            document.getElementById("clap").style.boxShadow = "gray 10px 30px 5px"
-            clap.play()
-        }else if (keypress === "KeyW" | e.target.id == "hihat"){
-            document.getElementById("hihat").style.boxShadow = "gray 10px 30px 5px"
-            hihat.play()
-        }else if (keypress === "KeyQ" | e.target.id == "snare"){
-            document.getElementById("snare").style.boxShadow = "gray 10px 30px 5px"
-            snare.play()
-        }else if (keypress === "KeyE" | e.target.id == "tink"){
-            document.getElementById("tink").style.boxShadow = "gray 10px 30px 5px"
-            tink.play()
-        }else if (keypress === "Space" | e.target.id == "kick"){
-            document.getElementById("kick").style.boxShadow = "gray 10px 30px 5px"
-            kick.play()
-        }else if (keypress === "KeyR" | e.target.id == "ride"){
-            document.getElementById("ride").style.boxShadow = "gray 10px 30px 5px"
-            ride.play()
-        }
-    
-    }
-}
 body.addEventListener("keyup", click)
 body.addEventListener("mouseup",click)
 
 
 function click(){
-document.getElementById("tom").style.boxShadow = "black 10px 30px 5px"
-document.getElementById("openhat").style.boxShadow = "black 10px 30px 5px"
-document.getElementById("clap").style.boxShadow = "black 10px 30px 5px"
-document.getElementById("hihat").style.boxShadow = "black 10px 30px 5px"
-document.getElementById("snare").style.boxShadow = "black 10px 30px 5px"
-document.getElementById("tink").style.boxShadow = "black 10px 30px 5px"
-document.getElementById("kick").style.boxShadow = "black 10px 30px 5px"
-document.getElementById("ride").style.boxShadow = "black 10px 30px 5px"
+document.getElementById("tom").style.boxShadow = "black 10px 20px 5px"
+document.getElementById("openhat").style.boxShadow = "black 10px 20px 5px"
+document.getElementById("clap").style.boxShadow = "black 10px 20px 5px"
+document.getElementById("hihat").style.boxShadow = "black 10px 20px 5px"
+document.getElementById("snare").style.boxShadow = "black 10px 20px 5px"
+document.getElementById("tink").style.boxShadow = "black 10px 20px 5px"
+document.getElementById("kick").style.boxShadow = "black 10px 20px 5px"
+document.getElementById("ride").style.boxShadow = "black 10px 20px 5px"
+}
+function playsound(e){
+    {
+        for (sound in sounds){
+            sounds[sound].pause()
+            sounds[sound].currentTime = 0;
+        }
+       let keypress = e.code
+        if (keypress === "KeyA" | e.target.id == "openhat"){
+            openhat.play()
+            document.getElementById("openhat").style.boxShadow = "gray 10px 20px 5px"
+
+        } else if (keypress === "KeyS" | e.target.id == "tom"){
+            document.getElementById("tom").style.boxShadow = "gray 10px 20px 5px"
+            tom.play()
+
+        }else if (keypress === "KeyD" | e.target.id == "clap"){
+            document.getElementById("clap").style.boxShadow = "gray 10px 20px 5px"
+            clap.play()
+        }else if (keypress === "KeyW" | e.target.id == "hihat"){
+            document.getElementById("hihat").style.boxShadow = "gray 10px 20px 5px"
+            hihat.play()
+        }else if (keypress === "KeyQ" | e.target.id == "snare"){
+            document.getElementById("snare").style.boxShadow = "gray 10px 20px 5px"
+            snare.play()
+        }else if (keypress === "KeyE" | e.target.id == "tink"){
+            document.getElementById("tink").style.boxShadow = "gray 10px 20px 5px"
+            tink.play()
+        }else if (keypress === "Space" | e.target.id == "kick"){
+            document.getElementById("kick").style.boxShadow = "gray 10px 20px 5px"
+            kick.play()
+        }else if (keypress === "KeyR" | e.target.id == "ride"){
+            document.getElementById("ride").style.boxShadow = "gray 10px 20px 5px"
+            ride.play()
+        }
+    
+    }
 }
