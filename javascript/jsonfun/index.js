a39a82cd4f10cb1600d281d4510e3c6c8c4703e4
christiankv <christian.kodehode@gmail.com>
2022-02-11 14:33:19 +0100

    a

--- javascript/jsonfun/index.js
+++ javascript/jsonfun/index.js
@@ -1,16 +1,34 @@
-// const myjson = fetch("caracter.json")
-//                 .then(response => response.json())
-//                 .then(json => console.log(json));
-//                 const caracterdata = JSON.parse(myjson)
-//                 const caracterdata = JSON.parse(myjson)
 
-// console.log(caracterdata);
+// let myjson = fetch("/json/caracter.json")
 
-async function getcaracterinfo(){
-    fetch("json/caracter.json")
-        .then(response => response.json())
-        .then(json => console.log(json));
+// console.log(myjson);
 
-}                
+// console.log(JSON.parse(myjson));
 
-getcaracterinfo()
+// async function getcaracterinfo(){
+//     fetch("https://christiankv.github.io/portfolio/javascript/jsonfun/json/caracter.json")
+//         .then(response => response.json())
+//         .then(json=> console.log(json))
+        
+
+// }
+// // fetch("https://christiankv.github.io/portfolio/javascript/jsonfun/json/caracter.json")
+// let carcterdata = 
+
+// getcaracterinfo()
+
+// // console.log(carcterdata);
+
+
+const myjson = `[
+    {"names":["christian","petter","ola"]},
+    {"class":["fighter","mage","thief"]},
+    {"fwep":["sword","2hsword"]},
+    {"mwep":["staff","orb","book"]},
+    {"twep":["dagger","bow"]}
+    ]`
+
+ let data = JSON.parse(myjson)
+
+
+console.log(data.names);
